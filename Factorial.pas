dd4543b2381a284ed6833db09712620ab0d5bb44
RTroshin <R.R.Troshin@outlook.com>
2020-03-06 12:05:06 +0300

    New file Factorial.pas

---
+++ Factorial.pas
@@ -0,0 +1,17 @@
+﻿{ Вычисление факториала }
+
+var
+  n, i, fact: integer;
+Begin
+  write('Введите число: ');
+  read(n);
+  if (n = 0) then
+  begin
+    write(n);
+    exit;
+  end;
+  fact := 1;
+  for i := 1 to n do
+    fact := fact * i;
+  writeln(n, '! = ', fact);
+end.
