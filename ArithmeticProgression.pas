c776e7b749acfe363fcf9c2ad1dce576b822f301
RTroshin <R.R.Troshin@outlook.com>
2020-03-10 10:10:10 +0300

    New file ArithmeticProgression.pas

---
+++ ArithmeticProgression.pas
@@ -0,0 +1,22 @@
+﻿{ Задание №4.1 }
+{ Составить программу для вычисления прогрессии }
+{ Арифметическая прогрессия }
+
+program ArithmeticProgression;
+var a, Sum, i, n, d: integer;
+Begin
+  write('Введите a: ');
+  read(a);
+  write('Введите n: '); { Диапазон арифметической прогрессии }
+  read(n);
+  write('Введите d: '); { Шаг арифметической прогрессии }
+  read(d);
+  Sum := a;
+  for i := 1 to n do
+  begin
+    Sum := Sum + d;
+    write(Sum, ' ');
+  end;
+  writeln;
+end.
+  
