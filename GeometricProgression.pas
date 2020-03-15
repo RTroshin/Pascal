f583a9c405cbb2831c8243592ffb8d0351939336
RTroshin <R.R.Troshin@outlook.com>
2020-03-15 11:15:00 +0300

    New file GeometricProgression.pas

---
+++ GeometricProgression.pas
@@ -0,0 +1,22 @@
+﻿{ Задание №4.2 }
+{ Составить программу для вычисления прогрессии }
+{ Геометричкая прогрессия }
+
+program GeometricProgression;
+var a, Sum, i, n, d: integer;
+Begin
+  write('Введите a: ');
+  read(a);
+  write('Введите n: '); { Диапазон геометрической прогрессии }
+  read(n);
+  write('Введите d: '); { Шаг геометрической прогрессии }
+  read(d);
+  Sum := a;
+  for i := 1 to n do
+  begin
+    Sum := Sum * d;
+    write(Sum, ' ');
+  end;
+  writeln;
+end.
+  
