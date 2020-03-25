163850d725134c53670cd5ddffcc732e14670a07
RTroshin <R.R.Troshin@outlook.com>
2020-03-25 16:00:42 +0300

    Update file Factorial.pas

--- Factorial.pas
+++ Factorial.pas
@@ -1,21 +1,14 @@
-﻿{ Задание №1 }
-{ Написать программу для определения факториала }
+﻿{ Вычисление факториала }
 
 program Factorial;
-var fact: longint; { Для вычисления факториала использовать тип longint }
-    i, n: byte; { Тип byte для неотрицательных значений }
-    stop: boolean;
+var
+  n, i, fact: integer;
 Begin
-  stop := false;
-  repeat
-    write('Введите число: ');
-    read(n);
-    if (n = 0) then
-      writeln(n);
-    fact := 1;
-    for i := 1 to n do
-      fact := fact * i;
-    writeln(n, '! = ', fact);
-    writeln;
-  until stop = true;
+  write('Введите число: ');
+  readln(n);
+  fact := 1;
+  for i := 1 to n do
+    fact := fact * i;
+  write(n, '! = ', fact);
+  readln;
 end.
