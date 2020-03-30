0182c8ec47ade690cd20179d6de6d464bf552a28
RTroshin <R.R.Troshin@outlook.com>
2020-03-30 21:45:14 +0300

    Update file QuadraticEquation.pas

--- QuadraticEquation.pas
+++ QuadraticEquation.pas
@@ -6,20 +6,25 @@ var a, b, c: integer;
     D, x, x1, x2: real;
 Begin
   write('Введите коэффициенты a, b и c квадратного уравнения ax^2 + bx + c: '); { Вывод строки с подсказкой }
-  read(a, b, c); { Считывание данных }
+  readln(a, b, c); { Считывание данных }
   D := b * b - 4 * a * c;
   if (D < 0) then
-    writeln('Нет решений!')
+  begin
+    write('Нет решений!');
+    readln;
+  end
   else
   if (D = 0) then
   begin
     x := -b div (2 * a);
-    writeln('Корень квадратного уравнения: x = ', x);
+    write('Корень квадратного уравнения: x = ', x);
+    readln;
   end;
   if (D > 0) then
   begin
     x1 := (-b + sqrt(D)) / (2 * a);
     x2 := (-b - sqrt(D)) / (2 * a);
-    writeln('Корни квадратного уравнения: x1 = ', x1, ' x2 = ', x2);
+    write('Корни квадратного уравнения: x1 = ', x1, ' x2 = ', x2);
+    readln;
   end;
 end.
