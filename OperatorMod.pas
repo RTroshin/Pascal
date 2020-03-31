d372f35cf5ecfeb7022b46da86be8c3ae4c8ceac
RTroshin <R.R.Troshin@outlook.com>
2020-03-31 17:15:14 +0300

    New file OperatorMod.pas

---
+++ OperatorMod.pas
@@ -0,0 +1,12 @@
+﻿{ Операция mod }
+
+program OperatorMod;
+var n, i: integer;
+Begin
+  write('Введите число n: ');
+  readln(n);
+  for i := 1 to n do
+    if n mod i = 0 then
+      write(i, ' ');
+  readln;
+end.
