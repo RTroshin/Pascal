80f3fac8e839f1146a33740a51a47f3cb892fe99
RTroshin <R.R.Troshin@outlook.com>
2020-04-01 12:40:00 +0300

    New file GuessNumber.pas

---
+++ GuessNumber.pas
@@ -0,0 +1,21 @@
+﻿{ Программа "Угадай число" }
+
+program GuessNumber;
+var a, b: integer;
+Begin
+  randomize;
+  a := random(100);
+  while a <> b do
+  begin
+    write('Введите число: ');
+    readln(b);
+    if b > a then
+      writeln('Много')
+    else
+      if b < a then
+        writeln('Мало')
+      else
+        writeln('Поздравляю! Вы угадали!');
+  end;
+  readln
+end.
