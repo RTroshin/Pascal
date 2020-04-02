ed1380495164a242469a98374842ee79945fc219
RTroshin <R.R.Troshin@outlook.com>
2020-04-02 14:20:03 +0300

    New file SearchNumber.pas

---
+++ SearchNumber.pas
@@ -0,0 +1,25 @@
+﻿{ Программа поиска первой цифры в числе n }
+
+program SearchNumber;
+var n, count, answer: integer;
+Begin
+  count := 0;
+  write('Введите число n: ');
+  readln(n);
+  answer := n;
+  repeat { Первый цикл определяет общее количество цифр в числе }
+    n := n div 10;
+    inc(count);
+  until (n = 0);
+  if (count >= 2) then
+  begin
+    repeat { Второй цикл определяет первую цифру в числе }
+      answer := answer div 10;
+      dec(count);
+    until (count = 1);
+    writeln(answer);
+  end
+  else
+    writeln(answer);
+  readln;
+end.
