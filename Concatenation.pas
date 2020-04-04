52491bb18a51a2d23930adf460a2748bbe20d19b
RTroshin <R.R.Troshin@outlook.com>
2020-04-04 17:05:07 +0300

    New file Concatenation.pas

---
+++ Concatenation.pas
@@ -0,0 +1,15 @@
+﻿{ Задание №2 }
+{ Провести конкатенацию строк имени и фамилии }
+
+program Concatenation;
+var str1, str2, str3: string;
+Begin
+  write('Введите имя: ');
+  readln(str1);
+  write('Введите фамилию: ');
+  readln(str2);
+  str3 := Concat(str1, ' ', str2);
+  writeln('-> ' + str3);
+  writeln(str2 + ' ' + str1 + ' <-');
+  readln;
+end.
