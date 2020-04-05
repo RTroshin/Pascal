3baebbcc6c80ab814432a49d0f6b1f0e8ab6c253
RTroshin <R.R.Troshin@outlook.com>
2020-04-05 23:50:16 +0300

    New file FunctionFactorial.pas

---
+++ FunctionFactorial.pas
@@ -0,0 +1,24 @@
+﻿{ Задание №2 }
+{ Составить программу со вспомогательным алгоритмом - функция }
+
+program FactorialFunction;
+var n: integer;
+function factorial(n: integer): longint;
+begin
+  if n = 0 then
+    factorial := 1
+  else
+    factorial := n * factorial(n - 1)
+end;
+Begin
+  while true do
+  begin
+    write('Введите число n: ');
+    readln(n);
+    if n < 0 then
+      writeln('Факториала не существует!')
+    else
+      writeln(n, '! = ', factorial(n));
+    writeln;
+  end;
+end.
