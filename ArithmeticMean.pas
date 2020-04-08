f66609d1282f5e4c668fee4782ae2d9b7a32f012
RTroshin <R.R.Troshin@outlook.com>
2020-04-08 22:40:45 +0300

    New file ArithmeticMean.pas

---
+++ ArithmeticMean.pas
@@ -0,0 +1,21 @@
+﻿{ Задание №1 }
+{ Составить программу со вспомогательным алгоритмом - процедура, для вывода
+  на экран среднего арифметического двух чисел }
+
+program ArithmeticMean;
+
+var AM: real; { Глобальная переменная }
+
+procedure ArithMean(a, b: integer);
+begin
+  AM := (a + b) / 2;
+end;
+
+var x, y: integer;
+Begin
+  write('Введите числа x и y: ');
+  readln(x, y);
+  ArithMean(x, y);
+  writeln('Среднее арифметическое равно ', AM);
+  readln;
+end.
