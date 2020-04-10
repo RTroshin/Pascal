31552b303339a7f678b429f38c183930c8ac2ecd
RTroshin <R.R.Troshin@outlook.com>
2020-04-10 20:45:25 +0300

    New file Summation.pas

---
+++ Summation.pas
@@ -0,0 +1,19 @@
+﻿{ Задание 2 }
+{ Составить программу со вспомогательным алгоритмом - функция, для вычисления
+  суммы  чисел - N }
+
+program Summation;
+
+function Sum(N: integer): longint;
+begin
+  if N > 0 then
+    Sum := N + Sum(N - 1);
+end;
+
+var N: integer;
+Begin
+  write('Введите число N: ');
+  readln(N);
+  writeln('Сумма чисел от 1 до N равна ', Sum(N));
+  readln;
+end.
