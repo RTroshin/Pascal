8b0fd41ae855ecaa827dcef80a57df03c3c5b563
RTroshin <R.R.Troshin@outlook.com>
2020-04-13 21:55:03 +0300

    New file EuclideanAlgorithm.pas

---
+++ EuclideanAlgorithm.pas
@@ -0,0 +1,19 @@
+﻿{ Задание №2 }
+{ Составить программу для нахождения наибольшего общего делителя.
+  Алгоритм Евклида }
+
+program EuclideanAlgorithm;
+var m, n: integer;
+Begin
+  write('Введите два числа для нахождения их наибольшего общего делителя: ');
+  readln(m, n);
+  while m <> n do
+  begin
+    if m > n then
+      m := m - n
+    else
+      n := n - m;
+  end;
+  writeln('Наибольший общий делитель: ', m);
+  readln;
+end.
