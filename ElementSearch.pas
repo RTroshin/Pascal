745ad5b6a9a3ffc2f69ef477457c3b7e63df4d66
RTroshin <R.R.Troshin@outlook.com>
2020-04-15 16:05:01 +0300

    New file ElementSearch.pas

---
+++ ElementSearch.pas
@@ -0,0 +1,27 @@
+﻿{ Задание №3 }
+{ Создать программу для поиска элемента в массиве }
+
+program ElementSearch;
+var A: array[1..5] of integer;
+    i, j, number: integer;
+Begin
+  j := 5;
+  write('Введите номер искомого элемента в массиве (от 1 до 5): ');
+  readln(number);
+  for i := 1 to 5 do
+  begin
+    A[i] := j;
+    write(A[i], ' ');
+    dec(j);
+  end;
+  writeln;
+  for i := 1 to 5 do
+  begin
+    if number = i then
+    begin
+      writeln('Найденный элемент: ', A[i]);
+      break;
+    end
+  end;
+  readln;
+end.
