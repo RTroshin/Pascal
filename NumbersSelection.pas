3959902dc3b35a3bbe5244141e3cf3e2f2499393
RTroshin <R.R.Troshin@outlook.com>
2020-04-18 23:05:01 +0300

    New file NumbersSelection.pas

---
+++ NumbersSelection.pas
@@ -0,0 +1,39 @@
+﻿{ Задание №5 }
+{ Создать программу для отбора нужных элементов в массиве }
+
+program NumbersSelection;
+
+const N = 10; { Константа для размерности массива }
+
+var A: array[1..N] of integer;
+    B: array[1..N] of integer;
+    i, count, userChoice: integer;
+Begin
+  writeln('Исходный массив:');
+  for i := 1 to N do
+  begin
+    A[i] := i;
+    write(A[i], ' ');
+  end;
+  count := 0;
+  writeln;
+  writeln;
+  writeln('Выберите 5 чисел из данного массива: ');
+  while count < 5 do
+  begin
+    readln(userChoice);
+    for i := 1 to N do
+    begin
+      if A[i] = userChoice then
+      begin
+        inc(count);
+        B[count] := A[i];
+      end;
+    end;
+  end;
+  writeln;
+  writeln('Выбранные числа:');
+  for i := 1 to count do
+    write(B[i], ' ');
+  readln;
+end.
