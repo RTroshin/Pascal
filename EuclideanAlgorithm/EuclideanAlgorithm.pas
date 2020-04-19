f0ebdf3c28df003c422f2b2a5e0eee4a7603f474
RTroshin <R.R.Troshin@outlook.com>
2020-04-19 23:25:14 +0300

    Program EuclideanAlgorithm 1.1
    Update file EuclideanAlgorithm.exe
    Update file EuclideanAlgorithm.pas

--- EuclideanAlgorithm/EuclideanAlgorithm.pas
+++ EuclideanAlgorithm/EuclideanAlgorithm.pas
@@ -3,17 +3,23 @@
   Алгоритм Евклида }
 
 program EuclideanAlgorithm;
+
+function Euclid(x, y: integer): integer;
+begin
+  while x <> y do
+  begin
+    if x > y then
+      x := x - y
+    else
+      y := y - x;
+  end;
+  Euclid := x;
+end;
+
 var m, n: integer;
 Begin
   write('Введите два числа для нахождения их наибольшего общего делителя: ');
   readln(m, n);
-  while m <> n do
-  begin
-    if m > n then
-      m := m - n
-    else
-      n := n - m;
-  end;
-  writeln('Наибольший общий делитель: ', m);
+  writeln('Наибольший общий делитель: ', Euclid(m, n));
   readln;
 end.
