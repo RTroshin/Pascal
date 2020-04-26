04936a0050461837e363e4755f30151546f55c25
RTroshin <R.R.Troshin@outlook.com>
2020-04-26 23:15:23 +0300

    Program ReverseArray 1.1
    Update file ReverseArray.exe
    Update file ReverseArray.pas

--- ReverseArray/ReverseArray.pas
+++ ReverseArray/ReverseArray.pas
@@ -1,25 +1,28 @@
-﻿{ Задание №5 }
+﻿{ Задание №4 }
 { Создать программу для создания реверса (перестановка элементов)в массиве }
 
 program ReverseArray;
-var A: array[1..10] of integer;
+
+const N = 10; { Константа для размерности массива }
+
+var A: array[1..N] of integer;
     i, temp: integer;
 Begin
   writeln('Массив в прямом порядке:');
-  for i := 1 to 10 do
+  for i := 1 to N do
   begin
     A[i] := i;
     write(A[i], ' ');
   end;
   writeln;
   writeln('Массив в обратном порядке:');
-  for i := 1 to 10 div 2 do
+  for i := 1 to N div 2 do
   begin
     temp := A[i];
-    A[i] := A[10 + 1 - i];
-    A[10 + 1 - i] := temp;
+    A[i] := A[N + 1 - i];
+    A[N + 1 - i] := temp;
   end;
-  for i := 1 to 10 do
+  for i := 1 to N do
     write(A[i], ' ');
   readln;
 end.
