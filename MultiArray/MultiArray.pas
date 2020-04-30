718aa6764374116ca028733e9a50cb190ac7bc49
RTroshin <R.R.Troshin@outlook.com>
2020-04-30 23:25:05 +0300

    Program MultiArray 1.1
    Update file MultiArray.exe
    Update file MultiArray.pas

--- MultiArray/MultiArray.pas
+++ MultiArray/MultiArray.pas
@@ -2,10 +2,10 @@
 { Составьте программу для представления двумерного массива, чтобы элементы 
   размещались по виду:
 
-  1 0 0 0  0 0 0 1  1 0 0 0
-  0 1 0 0  0 0 1 0  1 1 0 0
-  0 0 1 0  0 1 0 0  1 1 1 0
-  0 0 0 1  1 0 0 0  1 1 1 1
+  1  0  0  0   0  0  0  1   1  0  0  0
+  0  1  0  0   0  0  1  0   1  1  0  0
+  0  0  1  0   0  1  0  0   1  1  1  0
+  0  0  0  1   1  0  0  0   1  1  1  1
 }
 
 program MultiArray;
@@ -17,48 +17,50 @@ var A: array[1..M, 1..N] of integer;
     C: array[1..M, 1..N] of integer;
     number, i, j, k, count: integer;
 Begin
+  
+  { Заполняем массивы нулями }
+  
+  for i := 1 to M do
+    for j := 1 to N do
+    begin
+      A[i, j] := 0;
+      B[i, j] := 0;
+      C[i, j] := 0;
+    end;
+
   writeln('Двумерный массив с элементами главной диагонали:');
   writeln;
   for i := 1 to M do
-    for j := 1 to N do
-      if (i = j) then
-        A[i, j] := 1
-      else
-        A[i, j] := 0;
+    A[i, i] := 1;
   for i := 1 to M do
   begin
     for j := 1 to N do
-      write(A[i, j], ' ');
+      write(A[i, j]:3);
     writeln;
   end;
   writeln;
+  
   writeln('Двумерный массив с элементами побочной диагонали:');
   writeln;
   for i := 1 to M do
-    for j := 1 to N do
-      if (i = N + 1 - j) then
-        B[i, j] := 1
-      else
-        B[i, j] := 0;
+    B[i, N + 1 - i] := 1;
   for i := 1 to M do
   begin
     for j := 1 to N do
-      write(B[i, j], ' ');
+      write(B[i, j]:3);
     writeln;
   end;  
   writeln;
+
   writeln('Двумерный массив с элементами главной диагонали и элементами под ней:');
   writeln;
   for i := 1 to M do
-    for j := 1 to N do
-      if (i >= j) then
-        C[i, j] := 1
-      else
-        C[i, j] := 0;
+    for j := 1 to i do
+      C[i, j] := 1;
   for i := 1 to M do
   begin
     for j := 1 to N do
-      write(C[i, j], ' ');
+      write(C[i, j]:3);
     writeln;
   end;    
   readln;
