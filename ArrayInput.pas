757d8b24473f3b48b2e4ee32c4100235e53f02af
RTroshin <R.R.Troshin@outlook.com>
2020-05-01 22:45:01 +0300

    New file ArrayInput.pas

---
+++ ArrayInput.pas
@@ -0,0 +1,21 @@
+﻿{ Ввод и вывод одномерного массива }
+
+program ArrayInput;
+
+const N = 5; { Константа для размерности массива }
+
+var A: array[1..5] of integer;
+    i, number: integer;
+Begin
+  writeln('Введите элементы массива:');
+  for i := 1 to N do
+  begin
+    readln(number);
+    A[i] := number;
+  end;
+  writeln;
+  writeln('Введенный массив:');
+  for i := 1 to N do
+    write(A[i], ' ');
+  readln;
+end.
