be4c426bd2ff043f0e058285ad305179e5d8de31
RTroshin <R.R.Troshin@outlook.com>
2020-05-04 23:45:18 +0300

    Program OperatorMod
    Update file OperatorMod.exe
    Update file OperatorMod.pas

--- OperatorMod/OperatorMod.pas
+++ OperatorMod/OperatorMod.pas
@@ -3,8 +3,10 @@
 program OperatorMod;
 var n, i: integer;
 Begin
-  write('Введите число n: ');
+  writeln('Введите натуральное число n: ');
   readln(n);
+  writeln;
+  writeln('Все делители числа от 1 до ', n, ':');  
   for i := 1 to n do
     if n mod i = 0 then
       write(i, ' ');
