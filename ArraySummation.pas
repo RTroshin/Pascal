55840dc5dc48c060f99a1094ecaa5625a39c643e
RTroshin <R.R.Troshin@outlook.com>
2020-05-15 23:50:01 +0300

    New file ArraySummation.pas

---
+++ ArraySummation.pas
@@ -0,0 +1,25 @@
+﻿{ Задание: заполнить массив случайными числами.
+  Вычислить сумму его элементов }
+
+program ArraySummation;
+
+const N = 5;
+
+var A: array[1..N] of integer;
+    i, sum: integer ;
+Begin
+  randomize;
+  sum := 0;
+  for i := 1 to N do
+  begin
+    A[i] := random(5) + 1;
+    write(A[i], ' ');
+  end;
+  for i := 1 to N do
+  begin
+    sum := sum + A[i];
+  end;
+  writeln;
+  writeln('Сумма элементов массива равна: ', sum);
+  readln;
+end.
