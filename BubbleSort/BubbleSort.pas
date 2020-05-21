5e0c84c7e72c0a12b81aa90415349395bf029201
RTroshin <R.R.Troshin@outlook.com>
2020-05-21 23:56:34 +0300

    Program BubbleSort 1.1
    Update file BubbleSort.exe
    Update file BubbleSort.pas

--- BubbleSort/BubbleSort.pas
+++ BubbleSort/BubbleSort.pas
@@ -6,6 +6,7 @@ const N = 5; { Константа для размерности массива }
 
 var A: array[1..N] of integer;
     i, j, temp: integer;
+    stop: boolean;
 Begin
   randomize;
   writeln('Исходный массив: ');
@@ -14,14 +15,19 @@ Begin
     A[i] := random(100) + 1;
     write(A[i], ' ');
   end;
-  for i := 1 to N - 1 do
-    for j := 1 to N - i do
-      if (A[j] > A[j + 1]) then
-      begin
-        temp := A[j];
-        A[j] := A[j + 1];
-        A[j + 1] := temp;
-      end;
+  repeat
+    stop := false;
+    for i := 1 to N - 1 do
+      for j := 1 to N - i do
+        if (A[j] > A[j + 1]) then
+        begin
+          temp := A[j];
+          A[j] := A[j + 1];
+          A[j + 1] := temp;
+          stop := true;
+        end;
+  until stop = true;
+  writeln;
   writeln;
   writeln('Отсортированный массив по возрастанию: ');
   for i := 1 to N do
