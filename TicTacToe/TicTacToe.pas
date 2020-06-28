3c8a127a93a43f3aae4bd900b2cee2b131769a24
RTroshin <R.R.Troshin@outlook.com>
2020-06-28 23:45:27 +0300

    Program TicTacToe 1.4
    Update file TicTacToe.exe
    Update file TicTacToe.pas

--- TicTacToe/TicTacToe.pas
+++ TicTacToe/TicTacToe.pas
@@ -11,15 +11,15 @@ type arrayOfChar = array [1..N, 1..N] of char;
 procedure userTurn(var A: arrayOfChar; r, o: byte);
 begin
   readln(r, o);
-  while (A[r, o] <> '#') do
-  //repeat
-  begin
+  //while (A[r, o] <> '#') do
+  repeat
+  //begin
     writeln;
     writeln('Клетка занята! Попробуйте ещё раз');
     write('Введите номер строки и столбца через пробел: ');
     readln(r, o);
-  end;
-  //until (A[r, o] <> 'X') and (A[r, o] <> 'O');
+  //end;
+  until (A[r, o] <> 'X') and (A[r, o] <> 'O');
   //until (A[r, o] = '#');
   A[r, o] := 'X';
 end;
