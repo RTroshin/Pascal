50f753ed333f17544acc501394871d48c41338dc
RTroshin <R.R.Troshin@outlook.com>
2020-09-20 23:45:30 +0300

    Program TicTacToe 1.8
    Update file TicTacToe.exe
    Update file TicTacToe.pas

--- TicTacToe/TicTacToe.pas
+++ TicTacToe/TicTacToe.pas
@@ -14,7 +14,8 @@ type arrayOfChar = array [1..N, 1..N] of char;
 procedure userTurn(var A: arrayOfChar; userChoice: char; r, o: byte);
 begin
   readln(r, o);
-  if ((r > 3) or (o > 3)) then // Сделать цикл
+  if ((r > 3) or (o > 3)) then
+  while ((r > 3) or (o > 3)) do
   begin
     writeln;
     writeln('Введите два числа в диапазоне от 1 до 3!');
